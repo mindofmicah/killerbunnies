d5ff1da88621eda04c4bf98e2607a3c9c5dbed2b
Micah <micah@whamdonk.com>
2014-02-21 10:47:48 -0600

    base file for requirejs

---
+++ kbscore/js/main.js
@@ -0,0 +1,83 @@
+require.config({
+    baseUrl:'js',
+    paths: {
+        underscore:'vendor/underscore-min',
+        backbone:'vendor/backbone-min'
+    },
+    shim: {
+        backbone : {
+            exports: 'Backbone',
+            deps: ['underscore']
+        }
+    }
+});
+
+
+define(['views/App', 'modules/history/main','modules/players/main','views/GameView', 'backbone', 'views/DeadBunnyPopup'], function (App, History, Player, GameView, Backbone, DBP) {
+    var app = new App;
+    var history_collection = History.Collection();
+
+
+    var players = Player.Collection();
+    var pmv = Player.View();
+    var game = new GameView({
+        collection:players
+    });
+ 
+     R = Backbone.Router.extend({
+    routes: {
+        '': function () {
+            app.show('pmv');
+        },
+        'game':function () {
+            app.show('game');
+        }
+    }
+});
+
+ $('a').click(function (e) {
+    e.preventDefault();
+    r.navigate('game',{trigger:true});
+});
+
+popup = new DBP({
+    collection:Player.Collection(),
+    history_collection:History.Collection()
+});
+
+ r =new R;
+Backbone.history.start();
+
+// Make sure we start on the home page
+r.navigate('', {trigger:true});
+ 
+ history_view = new HistoryView({
+    collection:history_collection
+});
+
+
+/*
+
+
+
+
+
+
+
+
+popup = new DeadBunnyPopup({
+    collection:players,
+    history_collection:history_collection
+});
+history_view = new HistoryView({
+    collection:history_collection
+});
+
+r =new R;
+Backbone.history.start();
+
+// Make sure we start on the home page
+r.navigate('', {trigger:true});
+
+*/
+});
