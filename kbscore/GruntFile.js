0ab6d2cb138796a62f547adc232777bec8059981
Micah <micah@whamdonk.com>
2014-02-27 15:01:23 -0600

    Started using Grunt for build scripts

---
+++ kbscore/GruntFile.js
@@ -0,0 +1,24 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        requirejs: {
+            compile : {
+                options : {
+                    appDir : 'app/',
+                    baseUrl : 'js',
+                    dir :'target/',
+                    mainConfigFile:'app/js/main.js',
+                    keepBuildDir:false,
+                    modules:[
+                    {name:'main'}
+                    ],
+                    logLevel:0,
+                    findNestedDependencies : true,
+                    fileExclusionRegExp:/^\./,
+                    inlineText:true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+}
