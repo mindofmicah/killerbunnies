a7864f847f70d95c3df5cc2731e6104a2d052dd6
Micah <micah@whamdonk.com>
2014-02-28 16:49:58 -0600

    Changed commands to only upload new / updated files

--- app/build/build.sh
+++ app/build/build.sh
@@ -9,4 +9,4 @@ find js/ -type f | egrep -v "(js/vendor/requirejs/require.js|js/main)" | xargs r
 find js/ -type d | xargs rmdir -p 2>/dev/null
 
 echo "Uploading files to server"
-rsync -avv . ninemilefilms@ninemilefilms.com:~/html/whamdonk/public/kb/
+rsync -rlpgoDv --size-only . ninemilefilms@ninemilefilms.com:~/html/whamdonk/public/kb/
