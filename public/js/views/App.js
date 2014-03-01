3ac5ed5fb22cd0abc698ce6e5c4932ae7ef678b7
michael eschbacher <michaeleschbacher@gmail.com>
2014-03-01 14:41:08 -0600

    WE can only play the game if there are players

--- public/js/views/App.js
+++ public/js/views/App.js
@@ -4,13 +4,20 @@ define(['backbone','modules/players/main','views/GameView', 'modules/rules/main'
 var App = Backbone.View.extend({
     el:$('#content'),
         show : function (path) {
-
             if (path === 'pmv') {
                 this.$el.find('.show').removeClass('show');;
                 Player.View().$el.addClass('show');
             } else if (path === 'game') {
+                var player_collection = Player.Collection();
+                if (player_collection.length == 0) {
+                    window.location.hash = '#';
+                    this.$el.find('.show').removeClass('show');
+                    Player.View().$el.addClass('show');
+                    
+                    return;
+                }
                 this.$el.find('.show').removeClass('show');;
-                v = new GameView({collection:Player.Collection(), rules:Rules.Collection()});
+                v = new GameView({collection:player_collection, rules:Rules.Collection()});
                 v.$el.addClass('show');
             } else if(path === 'rules') {
                 this.$el.find('.show').removeClass('show');
