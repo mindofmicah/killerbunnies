5c8a67179e37ce3636d3c1a83682a7ed04476ec8
michael eschbacher <michaeleschbacher@gmail.com>
2014-03-02 20:31:09 -0600

    Copy over the app directory

--- build/build.sh
+++ build/build.sh
@@ -1,12 +1,16 @@
 grunt requirejs
 
-cd target
+cd target/public
 
 rm -rf build build.txt GruntFile.js
 
 echo "Removing unneeded files"
 find js/ -type f | egrep -v "(js/vendor/requirejs/require.js|js/main)" | xargs rm -rf
 find js/ -type d | xargs rmdir -p 2>/dev/null
+
+echo "Copy over the app directory"
+cp -av ../../app ../
+
 
 echo "Uploading files to server"
 rsync -rlpgoDv --size-only . ninemilefilms@ninemilefilms.com:~/html/whamdonk/public/kb/
