139fc399282a0f2cc64d92d92c85cd0e9f98af74
Micah <micah@whamdonk.com>
2014-03-03 16:17:21 -0600

    Fixed pathing issues, only copy over changes app files

--- build/build.sh
+++ build/build.sh
@@ -10,8 +10,8 @@ find js/ -type f | egrep -v "(js/vendor/requirejs/require.js|js/main)" | xargs r
 find js/ -type d | xargs rmdir -p 2>/dev/null
 
 echo "Copy over the app directory"
-cp -av ../../app ../
+cp -auv ../../app ../
 
 
 echo "Uploading files to server"
-rsync -rlpgoDv --size-only . ninemilefilms@ninemilefilms.com:~/html/killerbunnies/
+rsync -rlpgoDv --size-only ../ ninemilefilms@ninemilefilms.com:~/html/killerbunnies/
