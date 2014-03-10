67dcfbf6d03841f609341a8c956bb956a2d9ee70
michael eschbacher <michaeleschbacher@gmail.com>
2014-03-09 21:59:18 -0500

    Fixed linting issues

--- public/js/modules/players/views/player_view.js
+++ public/js/modules/players/views/player_view.js
@@ -1,38 +1,35 @@
-define(['backbone','modules/players/models/player'], function(Backbone, Player){
-    PlayerManagementView = Backbone.View.extend({
-    el:$('#player-management'), 
-    events: {
-        'submit form' : 'addPlayer',
-        'focus input' : 'removeErrors'
+define(['jquery', 'backbone', 'modules/players/models/player'], function ($, Backbone, Player) {
+    var PlayerManagementView = Backbone.View.extend({
+        el: $('#player-management'),
+        events: {
+            'submit form': 'addPlayer',
+            'focus input': 'removeErrors'
+        },
+        initialize: function () {
+            this.input = $('input[type="text"]');
+            this.listenTo(this.collection, 'add', this.render);
+            this.panel = $('#player-list-group');
+        },
+        render: function () {
+            var panel = this.panel;
 
-    },
-    initialize : function () {
-        this.input = $('input[type="text"]');
-
-        this.listenTo(this.collection, 'add', this.render);
-        this.panel = $('#player-list-group');
-    },
-    render: function () {
-        var panel = this.panel;
-    
-        panel.empty();
-        this.collection.each(function(a,b,c) {
-        panel.append('<li class="list-group-item">'+a.get('name')+'</li>');
-        });
-    },
-
-    addPlayer : function (e) {
-        e.preventDefault();
-        if (this.input.val() != '') {
-            this.collection.add(new Player({name:this.input.val()}));
-            this.input.val('').trigger('focus');
-        } else {
-            this.input.closest('.form-group').addClass('has-error');
+            panel.empty();
+            this.collection.each(function (a) {
+                panel.append('<li class="list-group-item">' + a.get('name') + '</li>');
+            });
+        },
+        addPlayer: function (e) {
+            e.preventDefault();
+            if (this.input.val() !== '') {
+                this.collection.add(new Player({name: this.input.val()}));
+                this.input.val('').trigger('focus');
+            } else {
+                this.input.closest('.form-group').addClass('has-error');
+            }
+        },
+        removeErrors: function () {
+            this.input.closest('.form-group').removeClass('has-error');
         }
-    },
-    removeErrors : function () {
-        this.input.closest('.form-group').removeClass('has-error');
-    }
-});
-return PlayerManagementView;
+    });
+    return PlayerManagementView;
 });
