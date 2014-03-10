636b507b9679f76beeb2ba094288ca7373b5c952
michael eschbacher <michaeleschbacher@gmail.com>
2014-03-09 21:18:21 -0500

    Added the rule definiton seeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -1,17 +1,19 @@
 <?php
 
-class DatabaseSeeder extends Seeder {
+class DatabaseSeeder extends Seeder
+{
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Eloquent::unguard();
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Eloquent::unguard();
         $this->call('GamesTableSeeder');
-		// $this->call('UserTableSeeder');
-	}
+        $this->call('Rule_definitionTableSeeder');
+        // $this->call('UserTableSeeder');
+    }
 
 }
