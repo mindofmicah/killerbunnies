89c70f6687d196e66774c6b22564b1d1aaca1747
michael eschbacher <michaeleschbacher@gmail.com>
2014-03-09 21:57:42 -0500

    Fixed linting issues

--- public/js/modules/players/collections/players.js
+++ public/js/modules/players/collections/players.js
@@ -1,36 +1,33 @@
 define(['backbone', 'modules/players/models/player'], function (Backbone, Player) {
-
-PlayerCollection = Backbone.Collection.extend({
-        model:Player,
-        getRankedModels : function () {
-
-            var m = this.models.sort(function (a,b) {
+    var PlayerCollection = Backbone.Collection.extend({
+        model: Player,
+        getRankedModels: function () {
+            var ranked_models = [],
+                current_rank = 0,
+                current_score,
+                ranks = [],
+                i;
+            ranked_models = this.models.sort(function (a, b) {
                 return a.get('score') < b.get('score');
             });
 
-            var current_rank = 0;
-            var current_score;;
-            var ranks = [];
-            for (var i = 0; i < m.length; i++) {
-
-                if (m[i].get('score') == current_score) {
+            for (i = 0; i < ranked_models.length; i++) {
+                if (ranked_models[i].get('score') === current_score) {
                     ranks.push({
-                        name:m[i].get('name'),
-                        rank:current_rank
-                    });                
-                } else {
-                    current_rank = i+1;
-                    ranks.push({
-                        name:m[i].get('name'),
-                        rank:current_rank
+                        name: ranked_models[i].get('name'),
+                        rank: current_rank
                     });
-                    current_score = m[i].get('score');
+                } else {
+                    current_rank = i + 1;
+                    ranks.push({
+                        name: ranked_models[i].get('name'),
+                        rank: current_rank
+                    });
+                    current_score = ranked_models[i].get('score');
                 }
-
             }
-
             return ranks;
         }
-    })
+    });
     return PlayerCollection;
 });
