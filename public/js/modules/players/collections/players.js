571fdf28f18828f400d1edfec56b601fb69eddbc
michael eschbacher <michaeleschbacher@gmail.com>
2014-03-11 23:27:05 -0500

    Created a method used to determine player rankings

--- public/js/modules/players/collections/players.js
+++ public/js/modules/players/collections/players.js
@@ -1,6 +1,24 @@
 define(['backbone', 'modules/players/models/player'], function (Backbone, Player) {
     var PlayerCollection = Backbone.Collection.extend({
         model: Player,
+        comparator: function (model) {
+            return -model.get('score');
+        },
+        calculateRanks: function(){
+            this.sort();
+            var current_rank = 0;
+            var current_score = null;
+            for(var i = 0; i<this.models.length; i++) {
+                if (this.models[i].get('score') === current_score ) {
+                    this.models[i].set('rank', current_rank);
+                } else {
+                    current_rank++;
+                    current_score = this.models[i].get('score');
+                    this.models[i].set('rank', current_rank);
+                }
+            }
+            return this;
+        },
         getRankedModels: function () {
             var ranked_models = [],
                 current_rank = 0,
