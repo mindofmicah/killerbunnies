d2a83c620b7979e028a1efb1ac49d9ff5b5e9cb9
Micah <micah@whamdonk.com>
2014-03-12 16:28:24 -0500

    Now allows to serve multiple views

--- public/js/modules/players/main.js
+++ public/js/modules/players/main.js
@@ -2,15 +2,16 @@ define([
     'modules/players/models/player',
     'modules/players/views/player_view',
     'modules/players/collections/players',
-    'modules/players/views/scoreboard_view'
-], function (M, V, C, S) {
+    'modules/players/views/scoreboard_view',
+    'modules/players/views/player_form'
+], function (M, V, C, S, FormView) {
     var collection = new C;
-    
+/*    
      var data = [
             {name:'mo',score:1},{name:'flett',score:4},{name:'andy',score:-3}
         ];
         collection = new C(data);
-    
+  */  
     var view = new V({
         collection:collection
     });
@@ -21,10 +22,16 @@ define([
         Model: function () {
             return new M;
         },
-        View: function (which_view) {
+        View: function (which_view, params) {
             if (which_view === 'scoreboard') {
                 return new S({collection:collection});
             }
+            if (which_view === 'form') {
+                params.collection = collection;
+                return new FormView(params);
+            }
+            which_view.collection=collection;
+            return new V(which_view);
             return view;
         }
     }
