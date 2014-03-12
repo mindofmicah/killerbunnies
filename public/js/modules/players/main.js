e7457fbd5d0bdab33cb88c7608ae352a838321f3
michael eschbacher <michaeleschbacher@gmail.com>
2014-03-11 22:54:48 -0500

    Allow grabbing the scoreboard view

--- public/js/modules/players/main.js
+++ public/js/modules/players/main.js
@@ -1,5 +1,16 @@
-define(['modules/players/models/player','modules/players/views/player_view','modules/players/collections/players'], function (m,v,c) {
+define([
+    'modules/players/models/player',
+    'modules/players/views/player_view',
+    'modules/players/collections/players',
+    'modules/players/views/scoreboard_view'
+], function (m,v,c, s) {
     var collection = new c;
+    
+     var data = [
+            {name:'mo',score:1},{name:'flett',score:4},{name:'andy',score:-3}
+        ];
+        collection = new c(data);
+    
     var view = new v({
         collection:collection
     });
@@ -10,7 +21,10 @@ define(['modules/players/models/player','modules/players/views/player_view','mod
         Model: function () {
             return new m;
         },
-        View: function () {
+        View: function (which_view) {
+            if (which_view === 'scoreboard') {
+                return new s({collection:collection});
+            }
             return view;
         }
     }
