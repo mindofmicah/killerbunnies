ae5a52bb498bfe7d6967132492afb44e024ca865
michael eschbacher <michaeleschbacher@gmail.com>
2014-03-11 22:58:40 -0500

    Basic view for the gameplay board

---
+++ app/views/games/play.blade.php
@@ -0,0 +1,8 @@
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="/css/main.css" />
+
+<script type="text/javascript"  data-main="/js/game_main" src="/js/vendor/requirejs/require.js"></script>
