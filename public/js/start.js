3112879177fe334f9427a5209dcfa6410c87bee5
Micah <micah@whamdonk.com>
2014-03-12 16:24:57 -0500

    Switched over to start using the startingPage view

--- public/js/start.js
+++ public/js/start.js
@@ -1,12 +1,15 @@
 require(['./common'], function (Common) {
     require([
         'modules/players/main',
+        'views/StartingPage',
         'backbone', 
         'bootstrap'
-    ], function (Player, Backbone, Bootstrap) { 
+    ], function (Player, StartingPage, Backbone, Bootstrap) { 
+        var starting_page = new StartingPage({
+                el:$('#landing-page')
+            });
 
-
-        var players = Player.Collection();
-        var pmv = Player.View();
+//        var players = Player.Collection();
+  //      var pmv = Player.View();
      });
 });
