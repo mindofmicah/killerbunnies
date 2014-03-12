cdfa9a21fc58523996202bebd308eeba63fe3610
michael eschbacher <michaeleschbacher@gmail.com>
2014-03-11 23:29:03 -0500

    JSlinting

--- public/js/modules/players/views/scoreboard_view.js
+++ public/js/modules/players/views/scoreboard_view.js
@@ -1,22 +1,20 @@
-define(['backbone','modules/players/views/scoreboard_list_view'], function (Backbone, ScoreboardListView) {
+define(['backbone', 'modules/players/views/scoreboard_list_view'], function (Backbone, ScoreboardListView) {
     var ScoreboardView = Backbone.View.extend({
-//        tagName: 'ul',
         className: 'list-group',
         initialize: function () {
             this.listenTo(this.collection, 'change', this.render);
             this.render();
         },
-      
         render: function () {
-            var standings = this.$el.empty();
+            var standings = this.$el.empty(),
+                scoreboard_list_view;
+
             this.collection.calculateRanks().each(function (model) {
-                scoreboard_list_view = new ScoreboardListView({model:model});
+                scoreboard_list_view = new ScoreboardListView({model : model});
                 standings.append(scoreboard_list_view.render().$el);
             });
 
             return this;
-        },
-        events: {
         }
 
     });
