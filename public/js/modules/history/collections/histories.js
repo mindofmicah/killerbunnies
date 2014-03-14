d0acdd4a6e49fe5a544cc09a5598985f61cde293
Micah <micah@whamdonk.com>
2014-03-14 15:40:07 -0500

    Linting

--- public/js/modules/history/collections/histories.js
+++ public/js/modules/history/collections/histories.js
@@ -1,33 +1,34 @@
 define(['backbone', 'modules/history/models/history'], function (Backbone, History) {
+    'use strict';
 
-var HistoryCollection = Backbone.Collection.extend({
-    model:History,
-    createNewHistory : function (p1, p2) {
-        if (p1 === p2) {
-            return this.createForFriendlyFire(p1, this.collection);
+    var HistoryCollection = Backbone.Collection.extend({
+        model:History,
+        createNewHistory : function (p1, p2) {
+            if (p1 === p2) {
+                return this.createForFriendlyFire(p1, this.collection);
+            }
+            return this.createForEnemy(p1, p2, this.collection);
+        },
+        createForEnemy: function (p1, p2, c) {
+            var message  = [
+                    p1.get('name'),
+                    ' killed one of ',
+                    p2.get('name'),
+                    '\'s bunnies. +1 point for ',
+                    + p1.get('name')
+                ].join('');
+
+            this.add(new History({message:message}));
+            p1.incrementScoreBy(1);
+        },
+        createForFriendlyFire : function (p1,c) {
+            var message = p1.get('name') + ' killed one of their own bunnies. -1 point';
+
+            this.add(new History({
+                message: message
+            }));
+            p1.decrementScoreBy(1);
         }
-        return this.createForEnemy(p1, p2, this.collection);
-    },
-    createForEnemy: function (p1, p2, c) {
-        var message  = [
-                p1.get('name'),
-                ' killed one of ',
-                p2.get('name'),
-                '\'s bunnies. +1 point for ',
-                + p1.get('name')
-            ].join('');
-
-        this.add(new History({message:message}));
-        p1.incrementScoreBy(1);
-    },
-    createForFriendlyFire : function (p1,c) {
-        var message = p1.get('name') + ' killed one of their own bunnies. -1 point'
-
-        this.add(new History({
-            message: message
-        }));
-        p1.decrementScoreBy(1);
-    }
-});
-return HistoryCollection
+    });
+    return HistoryCollection;
 });
