e087136790805f3eb9a59b2275806fd9d1b02344
Micah <micah@whamdonk.com>
2014-03-14 14:14:40 -0500

    JSHinting

--- public/js/views/DeadBunnyPopup.js
+++ public/js/views/DeadBunnyPopup.js
@@ -1,48 +1,47 @@
+/*globals define*/
 define(['backbone'], function (Backbone) {
+    'use strict';
 
+    var DeadBunnyPopup = Backbone.View.extend({
 
-var DeadBunnyPopup = Backbone.View.extend({
+            el : $('#dead-bunny-popup'),
+            initialize : function (params) {
+                this.render();
+                this.history_collection = params.history_collection;
+                this.collection = params.collection;
+                this.listenTo(this.collection, 'add', this.render);
+                this.selects = {
+                    killer : this.$el.find('#killer'),
+                    owner : this.$el.find('#owner')
+                };
+            },
+            events : {
+                'submit #dead-bunny' : 'markAsKilled'
+            },
+            markAsKilled : function (evt) {
+                evt.preventDefault();
+                var killer = this.selects.killer.val();
+                var owner = this.selects.owner.val();
 
-    el : $('#dead-bunny-popup'), 
-    initialize : function (params) {
-        this.render();
-        this.history_collection = params.history_collection;
-        this.collection = params.collection;
-        this.listenTo(this.collection, 'add', this.render);
-        this.selects = {
-            killer : this.$el.find('#killer'),
-            owner : this.$el.find('#owner')
-        }
-        
+                this.history_collection.createNewHistory(
+                    this.collection._byId[killer],
+                    this.collection._byId[owner]
+                );
+                this.$el.modal('hide');
+            },
+            fillSelectFromCollection : function ($select, collection) {
+                $select.empty();
+                collection.each(function (model) {
+                    $select.append('<option value="' + model.cid + '">' + model.get('name') + '</option>');
+                });
+          
+            },
+            render : function () {
+                for(var key in this.selects) {
+                    this.fillSelectFromCollection(this.selects[key], this.collection);
+                }
+            }
+        });
 
-
-    },
-    events : {
-        'submit #dead-bunny' : 'markAsKilled'
-    },
-    markAsKilled : function (evt) {
-        evt.preventDefault();
-        var killer = this.selects.killer.val();
-        var owner = this.selects.owner.val();
-
-
-        this.history_collection.createNewHistory(
-            this.collection._byId[killer],
-            this.collection._byId[owner]
-        );
-        this.$el.modal('hide');
-    },
-    render : function () {
-        for(var key in this.selects) {
-            var $select = this.selects[key].empty();
-
-            this.collection.each(function (model) {
-                $select.append('<option value="' + model.cid + '">' + model.get('name') + '</option>');
-            });
-        } 
-    }
-});
-
-
-return DeadBunnyPopup;
+    return DeadBunnyPopup;
 });
