0fbb1fa72802c5560d03664f58bacb08875fdace
Micah <micah@whamdonk.com>
2014-03-18 09:05:46 -0500

    Test file for the history module

---
+++ tests/modules/history/mainSpec.js
@@ -0,0 +1,48 @@
+define(['backbone','modules/history/main'], function (Backbone, History) {
+    describe('HistoryModule', function () {
+        it('should exist', function () {
+            expect(History).toBeDefined();
+        });
+        describe('Collection Method', function () {
+            it('should exist', function () {
+                expect(History.Collection).toBeDefined();    
+            });
+            it('should return a collection', function () {
+                expect(History.Collection() instanceof Backbone.Collection).toEqual(true);
+            });
+            it('should return the same collection by default', function () {
+                var collection_1 = History.Collection();
+                var collection_2 = History.Collection();
+                expect(collection_1).toEqual(collection_2);
+            });
+            it('should return the same collection by default even if I change a variable', function() {
+                var collection_1 = History.Collection();
+                collection_1.add({});
+                var collection_2 = History.Collection();
+                expect(collection_1).toEqual(collection_2);
+            });
+        });
+        describe('Model Method', function () {
+            it('should exist', function () {
+                expect(History.Model).toBeDefined();    
+            });
+            it('should return a Backbone Model', function () {
+                expect(History.Model() instanceof Backbone.Model).toEqual(true);
+            });
+            it('should return different models each time', function () {
+                var model_1 = History.Model();
+                var model_2 = History.Model();
+                expect(model_1).not.toEqual(model_2);
+            });
+        });
+        describe('View Method', function () {
+            it('should exist', function () {
+                expect(History.View).toBeDefined();    
+            });
+
+            it('should return a Backbone View', function () {
+                expect(History.View() instanceof Backbone.View).toBe(true);
+            });
+        });
+    });    
+});
